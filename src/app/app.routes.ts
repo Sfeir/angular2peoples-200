12483c2ad59091e083ffb13036bba7f1c200963a
Wassim CHEGHAM <github@wassimchegham.com>
2016-06-20 18:12:02 +0200

    chore: fix routing to match alpha.7

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 import { PeopleComponent } from './+people/';
+import { HomeComponent } from './home/';
 
 export const routes: RouterConfig = [
-  {path: '/people', component: PeopleComponent},
-  {path: '/people/:email', component: PeopleComponent}
+  {path: '', redirectTo: 'people'},
+  {path: 'people', component: HomeComponent},
+  {path: 'people/:email', component: PeopleComponent}
 ];
 
 export const APP_ROUTER_PROVIDERS = [
