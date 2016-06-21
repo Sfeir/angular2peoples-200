d4b8e93d7f9ed5740a990f156e7b123e1f97a3e9
Wassim CHEGHAM <github@wassimchegham.com>
2016-06-21 13:06:18 +0200

    chore: fix people component

--- src/app/+people/people.component.ts
+++ src/app/+people/people.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { PeopleCardComponent } from '../people-card/';
+import { PeopleService } from '../people.service';
 
 @Component({
   moduleId: module.id,
   selector: 'app-people',
   templateUrl: 'people.component.html',
   styleUrls: ['people.component.css'],
-  directives: [PeopleCardComponent]
+  directives: [PeopleCardComponent],
+  providers: [PeopleService]
 })
 export class PeopleComponent implements OnInit {
 
-  constructor() {}
+  private people: any = [];
+
+  constructor(private ppl: PeopleService) {}
 
   ngOnInit() {
+    this.ppl.fetch().subscribe((people) => this.people = people);
   }
 
 }
