b1d6074c8938df4f7df0301258c171d666045a00
Wassim CHEGHAM <github@wassimchegham.com>
2016-06-21 13:05:56 +0200

    chore: add random fetch call in people service

--- src/app/people.service.ts
+++ src/app/people.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/combineLatest';
 
 @Injectable()
 export class PeopleService {
@@ -8,8 +9,13 @@ export class PeopleService {
   constructor(private http: Http) {}
 
   fetch() {
-    return this.http.get('http://people.sfeir.com/person/all')
+    return this.http.get('http://localhost:3001/api/peoples/')
       .map( res => res.json() );
   }
 
+  fetchRandom() {
+    return this.http.get('http://localhost:3001/api/peoples/')
+      .map( res => res.json() )
+      .combineLatest( people => people[Math.floor(Math.random() * people.length)] );
+  }
 }
