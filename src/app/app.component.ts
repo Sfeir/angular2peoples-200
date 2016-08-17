41e167ba9d87e9e10d9b92272bd702047e2fdfea
Wassim CHEGHAM <github@wassimchegham.com>
2016-08-17 16:40:20 +0200

    chore: step-0a-solution: done

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'sfeir-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
+})
+export class PeopleAppComponent {
+  name = 'Angular 2';
+}
