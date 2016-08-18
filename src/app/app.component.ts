6fb9331dcbcafc30c6ba5df1e24725d4edb79459
Wassim CHEGHAM <github@wassimchegham.com>
2016-08-18 14:50:45 +0200

    chore: step-01a-solution: done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/';
 
 @Component({
   moduleId: module.id,
   selector: 'sfeir-app',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [HomeComponent]
 })
 export class PeopleAppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MD_MODULES } from './md.module';
 
-import { HomeComponent } from './home/';
 import { PeopleAppComponent } from './app.component';
 
 @NgModule({
@@ -12,12 +11,11 @@ import { PeopleAppComponent } from './app.component';
     ...MD_MODULES
   ],
   declarations: [
-    PeopleAppComponent,
-    HomeComponent
+    PeopleAppComponent
   ],
   providers: [ ],
   bootstrap: [
-    HomeComponent
+    PeopleAppComponent
   ]
 })
 export class AppModule { }
